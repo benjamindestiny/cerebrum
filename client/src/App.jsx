44de1663b76666065e1970b5b2f784c75cc8e4e6
benjamin destiny <benjamindestiny449@gmail.com>
2026-07-10 11:30:35 +0000

    update: added achievement to protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -86,6 +86,14 @@ function App() {
               }
             />
             <Route
+              path="achievements"
+              element={
+                <ProtectedRoute>
+                  <Achievements />
+                </ProtectedRoute>
+              }
+            />
+            <Route
               path="quiz"
               element={
                 <ProtectedRoute>
